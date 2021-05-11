6ad45f3a752ac28501d7e06bcc414a63bcf63560
jiyun0524 <jiyun9705@gmail.com>
2021-05-11 18:20:03 +0900

    0511
    test

---
+++ D_Proj/src/component/App.jsx
@@ -0,0 +1,22 @@
+// app.jsx
+import React from 'react';
+
+function App() {
+	return (
+		<body>
+			<header>
+				<h1>Board</h1>
+			</header>
+			<nav>
+				<ul>
+					<li><a href="#">intro</a></li>
+					<li><a href="#">hobby</a></li>
+					<li><a href="#">works</a></li>
+					<li><a href="#">friends</a></li>
+				</ul>
+			</nav>
+		</body>
+	)
+}
+
+export default App;
