06bb448a1800fc565c6760f2d84b38352fcbbc91
jiyun0524 <49427140+jiyun0524@users.noreply.github.com>
2021-05-13 23:37:26 +0900

    0513
    나눠서올릴게요

---
+++ 7_React/0513/src/components/RouterHookSample.jsx
@@ -0,0 +1,10 @@
+// RouterHookSample.jsx
+import useReactRouter from 'use-react-router';
+
+function RouterHookSample() {
+	const { history, location, match } = useReactRouter;
+	console.log({ history, location, match });
+	return null;
+}
+
+export default RouterHookSample;
