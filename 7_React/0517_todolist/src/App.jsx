d2739e5707e3f258fe506223b081533bba4bc867
jiyun0524 <49427140+jiyun0524@users.noreply.github.com>
2021-05-18 00:59:45 +0900

    0518
    todolist

--- 7_React/0517_todolist/src/App.jsx
+++ 7_React/0517_todolist/src/App.jsx
@@ -2,12 +2,23 @@
 import React, { Component } from 'react';
 import TodoListTemplate from './components/TodoListTemplate';
 import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
 
 class App extends Component {
+	id = 3 // 이미 0,1,2 가 존재하므로
+	state = {
+		input: '',
+		todos: [
+			{ id: 0, text: '리액트 소개', checked: false },
+			{ id: 1, text: '리액트 구조', checked: true },
+			{ id: 2, text: '리액트 사용', checked: false }
+		]
+	}
+
 	render() {
 		return (
 			<TodoListTemplate form={<Form />}>
-				제가....보이시나요........~~~
+				<TodoItemList /> 
 			</TodoListTemplate>
 		)
 	}
--- 7_React/0517_todolist/src/components/TodoItemList.jsx
+++ 7_React/0517_todolist/src/components/TodoItemList.jsx
@@ -1,6 +1,19 @@
 // src/components/TodoItemList.jsx
-import React, { components, Component } from 'react';
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
 
 class TodoItemList extends Component{
-	
+	render() {
+		const { todos, onToggle, onRemove } = this.props;
+
+		return (
+			<div>
+				<TodoItem text="하이"/>
+				<TodoItem text="만나서"/>
+				<TodoItem text="반갑습니뎅"/>				
+			</div>
+		)
+	}
 }
+
+export default TodoItemList;
