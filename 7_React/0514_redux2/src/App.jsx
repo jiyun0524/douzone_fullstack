e5ccdf190626d609788a556db259902c2155f3aa
jiyun0524 <jiyun9705@gmail.com>
2021-05-17 12:05:16 +0900

    0517
    0514_redux2 추가
    0517 생성
    0517_todolist 생성

--- 7_React/0514_redux2/src/App.jsx
+++ 7_React/0514_redux2/src/App.jsx
@@ -5,7 +5,7 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				
+				<h1>App</h1>
 			</div>
 		)
 	}
--- 7_React/0514_redux2/src/index.js
+++ 7_React/0514_redux2/src/index.js
@@ -1,15 +1,20 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import rootReducer from './modules';
 
-
 const store = createStore(rootReducer); // store 생성
-console.log(store.getState());
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 
 // serviceWorker.unregister();
